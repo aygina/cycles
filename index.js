d2a8286b62af8b0c7b03d14c2837d9f014f3c6bf
aygina <aygina.v@gmail.com>
2022-11-06 17:32:09 +0300

    done

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-let numbers = []; 
-
-// function save() {
-//     numbers[0] = document.getElementById("1").value;
-//     numbers[1] = document.getElementById("2").value;
-//     numbers[2] = document.getElementById("3").value;
-//     numbers[3] = document.getElementById("4").value;
-
-//     console.log(numbers);
-// }
+let numbers = [];
+let showNumbers = [];
 
 let input = document.getElementById("1");
 input.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
-        numbers.push = Number(document.getElementById("1").value);
+        numbers.push(Number(document.getElementById("1").value));
         console.log(numbers);
-        document.getElementById("1").value ='';
-    }        
+        document.getElementById("1").value = '';
+    }
 });
 
 function sumInput() {
-    alert(numbers);
+    function sortNumbers(a, b) {
+        if (a > b) {
+            return 1;
+        } else if (b > a) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+    numbers.sort(sortNumbers);
+    for (let i = 0; i < numbers.length; i++) {
+        showNumbers = showNumbers + numbers[i] + ` `;
+    }
+
+    let result = numbers.reduce(function (sum, current) {
+        return sum + current;
+    }, 0);
+
+    alert(`Массив отсортирован:  ` + showNumbers + `\n` + `Сумма массива:  ` + result );
 }
